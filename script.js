e9661b12459d945140fe0496248502db691b1577
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2024-04-01 21:01:35 -0300

    comecando js

---
+++ script.js
@@ -0,0 +1,4 @@
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const filterBtn = document.querySelector("#filter-select");
